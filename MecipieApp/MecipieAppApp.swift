b4c55be06229774146ea2dd9ebf5e68edbb32895
Marvis Ighedosa <dosamarvis@gmail.com>
2023-11-29 10:13:41 +0100

    Initial Commit

---
+++ MecipieApp/MecipieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MecipieAppApp.swift
+//  MecipieApp
+//
+//  Created by Marvis Ighedosa on 29/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MecipieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
